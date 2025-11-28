7be13b120cd8170e19494104072061eda70c6df6
Cody Emami <user@Codys-MacBook-Air.local>
2025-11-28 00:34:20 -0800

    update

--- pages/api/comments/on/[entityType]/[entityId].ts
+++ pages/api/comments/on/[entityType]/[entityId].ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-const { getDb } = require('../../../../shared/utils/db');
-const { getUserFromRequest } = require('../../../../shared/utils/auth');
+const { getDb } = require('../../../../shared/utils/db.js');
+const { getUserFromRequest } = require('../../../../shared/utils/auth.js');
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { entityType, entityId } = req.query;
