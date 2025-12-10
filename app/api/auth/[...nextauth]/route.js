559efcb44e3670ba023a7044524554177aab2724
CEMAMI09 <kryptctttv@gmail.com>
2025-12-10 15:34:24 -0800

    update

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,4 @@
 import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-import GitHubProvider from 'next-auth/providers/github';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { getDb } from '../../../../db/db';
 import { verifyPassword, generateToken } from '../../../../backend/lib/auth';
